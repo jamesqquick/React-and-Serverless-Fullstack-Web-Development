5dbc706f942357a01cbde42e2f38107e498fe61d
James Quick <james.q.quick@outlook.com>
2020-05-02 08:14:12 -0500

    Added part 8

---
+++ 8. Style the Home Page/src/styled/Random.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Accent = styled.span`
+    color: #e16365;
+`;
+
+export const StyledTitle = styled.h1`
+    font-size: 4.25rem;
+    font-weight: 300;
+    text-align: center;
+    margin-bottom: 2rem;
+    margin-top: 3rem;
+`;
