731ee89b509bbe5e75cc37d162b34a8bbd7b926e
James Quick <james.q.quick@outlook.com>
2020-05-15 07:42:44 -0500

    added 6.8

---
+++ 6.8 Refactoring Common Airtable Code/functions/saveHighScore.js
@@ -0,0 +1,52 @@
+const { table, getHighScores } = require('./utils/airtable');
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ err: 'That method is not allowed' }),
+        };
+    }
+
+    const { score, name } = JSON.parse(event.body);
+    if (typeof score === 'undefined' || !name) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ err: 'Bad request' }),
+        };
+    }
+
+    try {
+        const records = await getHighScores(false);
+
+        const lowestRecord = records[9];
+        if (
+            typeof lowestRecord.fields.score === 'undefined' ||
+            score > lowestRecord.fields.score
+        ) {
+            //update this record with the incoming score
+            const updatedRecord = {
+                id: lowestRecord.id,
+                fields: { name, score },
+            };
+            await table.update([updatedRecord]);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(updatedRecord),
+            };
+        } else {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                err: 'Failed to save score in Airtabl',
+            }),
+        };
+    }
+};
---
+++ 6.8 Refactoring Common Airtable Code/src/pages/HighScores.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { ScoresList, ScoreLI } from '../styled/HighScores';
+import { StyledTitle } from '../styled/Random';
+
+export default function HighScores() {
+    //display those scores
+    const [highScores, setHighScores] = useState([]);
+
+    //use the fetch API to call getHighScores function
+    useEffect(() => {
+        const loadHighScores = async () => {
+            try {
+                const res = await fetch('/.netlify/functions/getHighScores');
+                const scores = await res.json();
+                setHighScores(scores);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        loadHighScores();
+    }, []);
+    return (
+        <div>
+            <h1>High Scores</h1>
+            <ScoresList>
+                {highScores.map((score, index) => (
+                    <ScoreLI key={score.id}>
+                        {index + 1}. {score.fields.name} - {score.fields.score}
+                    </ScoreLI>
+                ))}
+            </ScoresList>
+        </div>
+    );
+}
