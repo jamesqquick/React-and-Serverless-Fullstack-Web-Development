1ad068ac62aabb74d9cb680a0eb27fc291f2999e
James Quick <james.q.quick@outlook.com>
2020-05-16 08:46:36 -0500

    removed margin top bug

--- 9.3 Use the Styled Components Theme Provider/src/styled/Container.js
+++ 9.3 Use the Styled Components Theme Provider/src/styled/Container.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
     padding: 20px;
     margin: 0 auto;
-    margin-top: 20px;
     max-width: 800px;
     text-align: center;
 `;
